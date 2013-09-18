5673e30b41bb1b1bcfeb2412a41230b99f9b2bfb
ihla <lubos.ilcik@gmail.com>
2013-09-18 21:13:50 +0200

    - custom shader test

--- grid-tests/src/co/joyatwork/grid/tests/CustomShader.java
+++ grid-tests/src/co/joyatwork/grid/tests/CustomShader.java
@@ -12,19 +12,34 @@ public class CustomShader extends BaseShader {
 
 	private final static String vertexShader = 
 			"attribute vec3 a_position;\n"+
+			"attribute vec3 a_normal;\n"+
 			"uniform mat4 u_projTrans;\n"+
 			"uniform mat4 u_worldTrans;\n"+
+			"uniform mat3 u_normalMatrix;\n"+
+			"uniform vec3 u_color;\n"+
+			"varying vec3 v_normal;\n"+
+			"varying vec3 v_diffuseColor;\n"+
+			"varying vec3 v_ambientLight;\n"+
+			"varying vec3 v_diffuseLight;\n"+
 			"void main() {\n"+
+			"	v_normal = normalize(u_normalMatrix * a_normal);\n"+
+			"	v_diffuseColor = u_color;\n"+
+			"	v_ambientLight = vec3(0.5, 0.5, 0.5);\n"+
+			"	v_diffuseLight = vec3(0.5, 0.5, 0.5);\n"+
 			"	gl_Position = u_projTrans * u_worldTrans * vec4(a_position, 1.0);\n"+
 			"}\n";
 	private final static String fragmentShader = 
+			"varying vec3 v_diffuseColor;\n"+
+			"varying vec3 v_ambientLight;\n"+
+			"varying vec3 v_diffuseLight;\n"+
 			"void main() {\n" +
-			"	gl_FragColor.rgb = vec3(0.2);\n" +
+			"	gl_FragColor.rgb = v_diffuseColor * (v_ambientLight + v_diffuseLight);\n" +
+			"	gl_FragColor.a = 1.0;\n" +
 			"}\n";
 	
 	protected final int u_projTrans	= register(new Uniform("u_projTrans"));
 	protected final int u_worldTrans = register(new Uniform("u_worldTrans"));
-	protected final int u_test = register(new Uniform("u_test"));
+	protected final int u_color = register(new Uniform("u_color"));
 	protected final ShaderProgram program;
 	
 	public CustomShader () {
@@ -43,6 +58,7 @@ public class CustomShader extends BaseShader {
 	public void begin (Camera camera, RenderContext context) {
 		program.begin();
 		set(u_projTrans, camera.combined);
+		set(u_color, 0.2f, 0.2f, 0.2f);
 	}
 
 	@Override
--- grid-tests/src/co/joyatwork/grid/tests/CustomShaderTest.java
+++ grid-tests/src/co/joyatwork/grid/tests/CustomShaderTest.java
@@ -59,7 +59,8 @@ public class CustomShaderTest implements ApplicationListener {
         camController.update();
          
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        //Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
  
         renderContext.begin();
         shader.begin(cam, renderContext);
